a8f2b60797e57df27b3a4b3be59e0d0fa24abcff
Mathias Bynens <mathias@qiwi.be>
2022-08-22 23:49:08 +0200

    Separate data update job from website build job

--- scrape.sh
+++ scrape.sh
@@ -3,4 +3,3 @@
 date="$(date +'%Y-%m-%d')";
 node get-bosses.mjs > "data/vunira/bosses-${date}.json";
 cp -r "data/vunira/bosses-${date}.json" "data/vunira/bosses-latest.json";
-node render-html.mjs;

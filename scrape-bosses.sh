51b87f10f6779634786652662b8e36a757de5e70
Mathias Bynens <mathias@qiwi.be>
2023-03-09 12:36:01 +0100

    Add Antica

--- scrape-bosses.sh
+++ scrape-bosses.sh
@@ -5,6 +5,7 @@ date="$(date +'%Y-%m-%d')";
 worlds=$(cat enabled-worlds.txt);
 for world in $worlds; do
 	world_slug="${world,,}";
+	mkdir -p "data/${world_slug}";
 	node get-bosses.mjs "${world}" > "data/${world_slug}/${date}.json";
 	ln --symbolic --force "${date}.json" "data/${world_slug}/latest.json";
 done;

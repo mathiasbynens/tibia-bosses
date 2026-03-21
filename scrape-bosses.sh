73f1b2ff91bd0006b09ebf1609f15ae193af3045
Tibia boss checker <bosses@tibiamaps.io>
2026-03-21 09:43:51 +0000

    Add boss data for date=2026-03-21

--- scrape-bosses.sh
+++ scrape-bosses.sh
@@ -4,7 +4,8 @@ date="$(date +'%Y-%m-%d')";
 
 worlds=$(cat enabled-worlds.txt);
 for world in $worlds; do
-	world_slug="${world,,}";
+	#world_slug="${world,,}";
+	world_slug="$(tr '[:upper:]' '[:lower:]' <<< "$world")";
 	mkdir -p "data/${world_slug}";
 	node get-bosses.mjs "${world}" > "data/${world_slug}/${date}.json";
 	ln --symbolic --force "${date}.json" "data/${world_slug}/latest.json";
